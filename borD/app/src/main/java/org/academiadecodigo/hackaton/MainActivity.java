74c00c21d1b1031da84dcf989814c7a29c407839
Pedro Lemos <pedrojblemos@gmail.com>
2020-04-08 16:28:30 +0100

    Added UI, start version.

--- borD/app/src/main/java/org/academiadecodigo/hackaton/MainActivity.java
+++ borD/app/src/main/java/org/academiadecodigo/hackaton/MainActivity.java
@@ -2,13 +2,27 @@ package org.academiadecodigo.hackaton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.scwang.wave.MultiWaveHeader;
 
 public class MainActivity extends AppCompatActivity {
+    MultiWaveHeader waveHeader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        waveHeader = findViewById(R.id.wave_header);
+
+        waveHeader.setVelocity(1);
+        waveHeader.setProgress(1);
+        waveHeader.setWaveHeight(85);
+        waveHeader.setStartColor(0xebebeb);
+        waveHeader.setCloseColor(0xababab);
+        waveHeader.isRunning();
     }
 }
